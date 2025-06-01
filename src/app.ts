891f025e86d8128091bc5a3a4d571b808bad2c86
junaidirah <junaidirahmat02@gmail.com>
2025-06-01 10:42:33 +0700

    debugger prisma

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import router  from "../src/routes/routes"
-
+import prisma from "./config/db";
 dotenv.config();
 
 const app = express();
@@ -28,6 +28,20 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/test-db', async (_req, res) => {
+  try {
+    const result = await prisma.$queryRaw`SELECT 1`;
+    res.json({ success: true, result });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ success: false, error: error.message });
+    } else {
+      res.status(500).json({ success: false, error: 'Unknown error occurred' });
+    }
+  }
+});
+
 
 app.get("/", (_req, res) => {
   res.send("Hello, World!");
